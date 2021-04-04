80f9b7c9887929d51053e749706a6f74f7ed1d9e
Miradouro <rafaelmiradouro@gmail.com>
2021-04-04 11:42:27 -0300

    adicionado permissão no ponto turistico e testado algumas

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,5 +1,7 @@
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
@@ -13,6 +15,11 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     serializer_class = PontoTuristicoSerializer
     filter_backends = [SearchFilter, ]
+    permission_classes = [DjangoModelPermissions, ]
+    #permission_classes = [IsAuthenticatedOrReadOnly, ]
+    #permission_classes = [IsAuthenticated, ]
+    #permission_classes = [IsAdminUser, ]
+    authentication_classes = (TokenAuthentication, )
     search_fields = ['nome', 'descricao', 'enderecos__linha1']
 
     def get_queryset(self):
