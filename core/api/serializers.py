d420330e43312c6d8cad3cb20b398afc4a649063
Miradouro <rafaelmiradouro@gmail.com>
2021-04-04 17:06:03 -0300

    conhecendo nested serializers

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
+from enderecos.api.serializers import EnderecoSerializer
+from avaliacoes.api.serializers import AvaliacaoSerializer
+from comentarios.api.serializers import ComentarioSerializer
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    enderecos = EnderecoSerializer()
+    avaliacoes = AvaliacaoSerializer(many=True)
+    comentarios = ComentarioSerializer(many=True)
+
     class Meta:
         model = PontoTuristico
-        fields = ['id', 'nome', 'descricao', 'aprovado', 'foto', 'enderecos']
+        fields = ['id', 'nome', 'descricao', 'aprovado', 'foto',
+                  'atracoes', 'comentarios', 'avaliacoes', 'enderecos']
 
