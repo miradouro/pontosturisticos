4ce0f9c773995a0af29f9b089ea5aeddbda23e27
Miradouro <rafaelmiradouro@gmail.com>
2021-04-04 17:25:18 -0300

    conhecendo serializer methodfield

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
 from atracoes.api.serializers import AtracaoSerializer
@@ -11,9 +12,14 @@ class PontoTuristicoSerializer(ModelSerializer):
     enderecos = EnderecoSerializer()
     avaliacoes = AvaliacaoSerializer(many=True)
     comentarios = ComentarioSerializer(many=True)
+    descricao_completa = SerializerMethodField()
 
     class Meta:
         model = PontoTuristico
         fields = ['id', 'nome', 'descricao', 'aprovado', 'foto',
-                  'atracoes', 'comentarios', 'avaliacoes', 'enderecos']
+                  'atracoes', 'comentarios', 'avaliacoes', 'enderecos',
+                  'descricao_completa', 'descricao_completa2']
+
+    def get_descricao_completa(self, obj):
+        return '%s - %s' % (obj.nome, obj.descricao)
 
